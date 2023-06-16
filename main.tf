b1e87ee90592dca6368eaab7570d59cb04b5e1c6
Adrian <github@surwave.ch>
2023-06-16 14:53:51 +0200

    Scratch

--- main.tf
+++ main.tf
@@ -1,125 +1,131 @@
-provider "azurerm" {
-  features {}
+resource "as-ffn-workshop" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.0"
-    }
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = as-ffn-workshop.rg_name.id
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
 
-variable "prefix" {
-  description = "Enter the prefix for the resource names"
-  type        = string
-  default     = "AS-FFN-Workshop"
-}
-
-variable "resource_group_name" {
-  description = "Enter the name for the resource group"
-  type        = string
-}
-
-resource "azurerm_resource_group" "ffn-workshop" {
-  name     = var.resource_group_name
-  location = "switzerlandnorth"
-}
-
-resource "azurerm_public_ip" "ffn-workshop" {
-  name                = "${var.prefix}-public-ip"
-  location            = azurerm_resource_group.ffn-workshop.location
-  resource_group_name = azurerm_resource_group.ffn-workshop.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_network_security_group" "ffn-workshop" {
-  name                = "${var.prefix}-nsg"
-  location            = azurerm_resource_group.ffn-workshop.location
-  resource_group_name = azurerm_resource_group.ffn-workshop.name
-}
-
-resource "azurerm_network_security_rule" "ffn-workshop" {
-  name                        = "SSH"
-  priority                    = 1001
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefixes      = ["0.0.0.0/0"]
-  destination_address_prefixes = ["*"]
-  resource_group_name         = azurerm_resource_group.ffn-workshop.name
-  network_security_group_name = azurerm_network_security_group.ffn-workshop.name
-}
-
-resource "azurerm_virtual_network" "ffn-workshop" {
-  name                = "${var.prefix}-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.ffn-workshop.location
-  resource_group_name = azurerm_resource_group.ffn-workshop.name
-}
-
-resource "azurerm_subnet" "ffn-workshop" {
-  name                 = "${var.prefix}-subnet"
-  resource_group_name  = azurerm_resource_group.ffn-workshop.name
-  virtual_network_name = azurerm_virtual_network.ffn-workshop.name
-  address_prefixes     = ["10.0.0.0/24"]
-}
-
-resource "azurerm_subnet_network_security_group_association" "ffn-workshop" {
-  subnet_name                 = "${var.prefix}-subnet"
-  network_security_group_name = azurerm_network_security_group.ffn-workshop.name
-}
-
-resource "azurerm_network_interface" "ffn-workshop" {
-  name                      = "${var.prefix}-nic"
-  location                  = azurerm_resource_group.ffn-workshop.location
-  resource_group_name       = azurerm_resource_group.ffn-workshop.name
+# Create network interface
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "${var.prefix}-ip-config"
-    subnet_id                     = azurerm_subnet.ffn-workshop.id
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.ffn-workshop.id
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "ffn-workshop" {
-  name                = "${var.prefix}-vm"
-  location            = azurerm_resource_group.ffn-workshop.location
-  resource_group_name = azurerm_resource_group.ffn-workshop.name
-  size                = "Standard_DS1_v2"
-  admin_username      = "adminuser"
-  admin_password      = random_password.ffn-workshop.result
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+}
 
-  network_interface_ids = [azurerm_network_interface.ffn-workshop.id]
+# Generate random text for a unique storage account name
+resource "random_id" "random_id" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 8
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "my_storage_account" {
+  name                     = "diag${random_id.random_id.hex}"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Create (and display) an SSH key
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
+  name                  = "myVM"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  size                  = "Standard_DS1_v2"
 
   os_disk {
+    name                 = "myOsDisk"
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "Premium_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
-}
 
-resource "random_password" "ffn-workshop" {
-  length           = 16
-  special          = true
-  override_special = "!@#"
-}
+  computer_name                   = "myvm"
+  admin_username                  = "azureuser"
+  disable_password_authentication = true
 
-output "vm_password" {
-  value = random_password.ffn-workshop.result
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.example_ssh.public_key_openssh
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
+  }
 }

ff6e18f3128a02ee75d2a3cac08c72ca8cd3dd5d
Adrian <github@surwave.ch>
2023-06-16 11:22:10 +0200

    Merge TF files

--- main.tf
+++ main.tf
@@ -13,12 +13,96 @@ variable "resource_group_name" {
   type        = string
 }
 
-module "fortinet_integration" {
-  source            = "./fortinet_integration"
-  prefix            = var.prefix
-  resource_group    = var.resource_group_name
+resource "azurerm_resource_group" "ffn-workshop" {
+  name     = var.resource_group_name
+  location = "switzerlandnorth"
+}
+
+resource "azurerm_public_ip" "ffn-workshop" {
+  name                = "${var.prefix}-public-ip"
+  location            = azurerm_resource_group.ffn-workshop.location
+  resource_group_name = azurerm_resource_group.ffn-workshop.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_security_group" "ffn-workshop" {
+  name                = "${var.prefix}-nsg"
+  location            = azurerm_resource_group.ffn-workshop.location
+  resource_group_name = azurerm_resource_group.ffn-workshop.name
+}
+
+resource "azurerm_network_security_rule" "ffn-workshop" {
+  name                        = "SSH"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefixes      = ["0.0.0.0/0"]
+  destination_address_prefixes = ["*"]
+  resource_group_name         = azurerm_resource_group.ffn-workshop.name
+  network_security_group_name = azurerm_network_security_group.ffn-workshop.name
+}
+
+resource "azurerm_virtual_network" "ffn-workshop" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.ffn-workshop.location
+  resource_group_name = azurerm_resource_group.ffn-workshop.name
+}
+
+resource "azurerm_subnet" "ffn-workshop" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.ffn-workshop.name
+  virtual_network_name = azurerm_virtual_network.ffn-workshop.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_network_interface" "ffn-workshop" {
+  name                      = "${var.prefix}-nic"
+  location                  = azurerm_resource_group.ffn-workshop.location
+  resource_group_name       = azurerm_resource_group.ffn-workshop.name
+  network_security_group_id = azurerm_network_security_group.ffn-workshop.id
+
+  ip_configuration {
+    name                          = "${var.prefix}-ip-config"
+    subnet_id                     = azurerm_subnet.ffn-workshop.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.ffn-workshop.id
+  }
+}
+
+resource "random_password" "ffn-workshop" {
+  length           = 16
+  special          = true
+  override_special = "!@#"
+}
+
+resource "azurerm_linux_virtual_machine" "ffn-workshop" {
+  name                = "${var.prefix}-vm"
+  location            = azurerm_resource_group.ffn-workshop.location
+  resource_group_name = azurerm_resource_group.ffn-workshop.name
+  size                = "Standard_DS1_v2"
+  admin_username      = "adminuser"
+  admin_password      = random_password.ffn-workshop.result
+
+  network_interface_ids = [azurerm_network_interface.ffn-workshop.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
 }
 
 output "vm_password" {
-  value = module.fortinet_integration.vm_password
+  value = random_password.ffn-workshop.result
 }
